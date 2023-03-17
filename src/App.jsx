232942787691f116a67f9d3f2e56c2ecd9267314
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-03-18 00:27:09 +0200

    __New__Commit__

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from "routes";
 function App() {
 	return (
 		<Routes>
+			<Route path="*" element="<h1>Route Not Defined</h1>" />
 			<Route path="/" element={<Home />} />
 		</Routes>
 	);
