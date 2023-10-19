33b18697b1221843e0d04ba1677020364bd7ed69
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-10-19 10:14:19 +0300

    Update useAxios.jsx

--- client/src/hooks/useAxios.jsx
+++ client/src/hooks/useAxios.jsx
@@ -9,34 +9,36 @@ else router = axios.create(routes.locale);
 export const useAxios = (method, url, body, options) => {
 	const [data, setData] = useState();
 	const [isSubmitted, setIsSubmitted] = useState(false);
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
 	const [status, setStatus] = useState(0);
 
 	const fetcher = async (method, url, body, options) => {
 		if (!method || url === "/") return;
 
+		setLoading(true);
+		setIsSubmitted(false);
+
 		try {
-			if (method === "get") {
-				const response = await router.get(url, options);
-				setData(() => response?.data);
-				setStatus(() => response?.status);
-				return { data: response?.data, loading: false, error: false, status: response.status, isSubmitted: true };
-			} else {
-				const response = await router[method](url, body, options);
-				setData(() => response?.data);
-				setStatus(() => response?.status);
-				return { data: response?.data, loading: false, error: false, status: response.status, isSubmitted: true };
-			}
+			let response;
+
+			if (method === "get") response = await router.get(url, options);
+			else response = await router[method](url, body, options);
+
+			setData(() => response?.data);
+			setStatus(() => response?.status);
+			setLoading(() => false);
+			setIsSubmitted(() => true);
+
+			return { data: response.data, loading: false, error: false, status: response.status, isSubmitted: true };
 		} catch (error) {
 			const err = error?.response?.data?.error || error?.message || "Network Error";
-			console.log(error);
+
 			setError(() => err);
 			setStatus(error?.status);
+
+			console.log(error);
 			return { data: null, loading: false, error: err, isSubmitted: true, status: error?.status };
-		} finally {
-			setLoading(false);
-			setIsSubmitted(true);
 		}
 	};
 
